8d20939974458fd69a11adacbbb7bcaa62bffc52
Lindsey <lle6138@rit.edu>
2013-12-15 13:58:10 -0500

    Readding helper function

--- ToTheSkies/ToTheSkies/ToTheSkies/Helper.c
+++ ToTheSkies/ToTheSkies/ToTheSkies/Helper.c
@@ -1,13 +1,29 @@
 //
-//  Helper.c
-//  ToTheSkies
-//
-//  Created by Student on 11/15/13.
-//  Copyright (c) 2013 Student. All rights reserved.
-//
-#include "Helper.h"
 
+#include "Helper.h"
+// Return a random number within a range, inclusive
 
 int randInRange(int lowerBound, int upperBound){
     return rand() % upperBound + lowerBound;
+    
+}
+
+// Using general form to calculate distance
+float calcDistance(cPoint start, cPoint end, cPoint playerPosition, float height)
+{
+      /* Getting general form from two points.
+      a = y1-y2,
+      b = x2-x1,
+      c = (x1-x2)*y1 + (y2-y1)*x1
+      */
+    
+    float a = start.y - end.y;
+    float b = end.x - start.x;
+    float c = (start.x - end.x) * start.y + (end.y - start.y) * start.x;
+    float x = playerPosition.x;
+    float y = -playerPosition.y + height;
+    
+    // Using general form to calculate distance
+    float distance = (fabsf(a*x+b*y+c))/(sqrtf(a*a+b*b));
+    return distance;
 }
